64929fd833ddf35a65a36dbb5831f70029a64015
satyajeetrai007 <satyajeetrai0@gmail.com>
2025-07-26 00:22:19 +0530

    removed linting mistakes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-def sum(a,b):
+def sum(a, b):
     return a+b
 
 
-def subtract(a,b):
+def subtract(a, b):
     return a-b
 
-result1 = sum(5,3)
-result2 = subtract(5,3)
+
+result1 = sum(5, 3)
+result2 = subtract(5, 3)
 
 print(result1,result2)
